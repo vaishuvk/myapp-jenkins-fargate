4945e09b4834655549d1e93de1d28fd51269e4f0
vaishuvk <71807351+vaishuvk@users.noreply.github.com>
2021-05-20 16:26:03 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,38 @@
+# network.tf
+
+# Fetch AZs in the current region
+data "aws_availability_zones" "available" {
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+}
+
+# Create var.az_count private subnets, each in a different AZ
+resource "aws_subnet" "public-1" {
+  count             = var.az_count
+  cidr_block        = "10.10.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  map_public_ip_on_launch = true
+}
+
+# Create var.az_count public subnets, each in a different AZ
+resource "aws_subnet" "public-2" {
+  count                   = var.az_count
+  cidr_block              = "10.10.3.0/24"
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+}
+
+# Internet Gateway for the public subnet
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route the public subnet traffic through the IGW
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
